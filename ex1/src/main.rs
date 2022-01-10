f25e04134cc7b57c9b79206be84428c7e540385f
Joshua Davis <pgmjsd@gmail.com>
2022-01-10 11:55:26 -0500

    clippy / rustfmt cleanup

--- ex1/src/main.rs
+++ ex1/src/main.rs
@@ -1,14 +1,13 @@
-
 fn calc(x: i32, f: f64) -> i32 {
     let z = x as f64 * f + 5.0;
     let y = z * 3.0;
-    return y.round() as i32;
+    y.round() as i32
 }
 
 fn main() {
     let mut r = 10;
     let arg = 22;
     println!("r={}", r);
-    r = calc(arg, std::f64::consts::PI );
+    r = calc(arg, std::f64::consts::PI);
     println!("r={}", r);
 }
